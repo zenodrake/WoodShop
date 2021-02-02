e034dd0706c566ba8c1debefd99666f6b3482fb1
zenodrake <zenodrake@gmail.ocm>
2021-02-01 23:19:01 -0700

    concrete steps now fall into one of three types: Generation, Alteration, or Assembly. also added is_completed attribute

--- crafting_steps.py
+++ crafting_steps.py
@@ -3,9 +3,11 @@ import time
 
 class Step:
     """The base class for all steps. Defines one method perform()"""
-    def __init__(self, name, **kwargs):
+    def __init__(self, name, step_type, **kwargs):
         self.name = name
+        self.step_type = step_type
         self.INITIATION_TIME = 2
+        self.__is_completed = False
 
     def perform(self):
         print(f'Initiating {self.name.lower()}....', end='')
@@ -21,12 +23,41 @@ class Step:
             time.sleep(1)
 
         print(f'{self.name} completed!')
+        self.set_completed()
+
+    @property
+    def is_completed(self):
+        return self.__is_completed
+
+    def set_completed(self):
+        self.__is_completed = True
 
     def __repr__(self):
         return self.name
 
 
-class CuttingStep(Step):
+class GenerationStep(Step):
+    def __init__(self, name, **kwargs):
+        self.name = name
+        self.step_type = 'generation'
+        super().__init__(self.name, self.step_type, **kwargs)
+
+
+class AlterationStep(Step):
+    def __init__(self, name, **kwargs):
+        self.name = name
+        self.step_type = 'alteration'
+        super().__init__(self.name, self.step_type, **kwargs)
+
+
+class AssemblyStep(Step):
+    def __init__(self, name, **kwargs):
+        self.name = name
+        self.step_type = 'assembly'
+        super().__init__(self.name, self.step_type, **kwargs)
+
+
+class CuttingStep(GenerationStep):
     """The ripping of a board into narrower sections or the cutting of boards into shorter sections.
     Also applies to using the scroll saw to cut out intricate shapes"""
     def __init__(self):
@@ -35,14 +66,15 @@ class CuttingStep(Step):
         super().__init__(self.name)
 
 
-class DrillingStep(Step):
+class DrillingStep(AlterationStep):
     """Drilling holes in things to prepare them for fastening together"""
     def __init__(self):
         self.name = 'Drilling'
         self.time_to_complete = 2
         super.__init__(self.name)
 
-class FasteningStep(Step):
+
+class FasteningStep(AssemblyStep):
     """The screwing or nailing of two pieces of wood together. Separate and distinct from Gluing, but can be used in
     combination"""
     def __init__(self):
@@ -51,7 +83,7 @@ class FasteningStep(Step):
         super().__init__(self.name)
 
 
-class GluingStep(Step):
+class GluingStep(AssemblyStep):
     """Attaching one piece of wood to another by chemical means. Can be used in conjunction with Gluing or used
     separately"""
     def __init__(self):
@@ -69,7 +101,7 @@ class GluingStep(Step):
         print('\nGlue is dry!')
 
 
-class JointingStep(Step):
+class JointingStep(AlterationStep):
     """Smoothing out and making parallel the two narrow sides of a board.
     Useful when making desks, table, cutting boards, drawers, and sofas"""
     def __init__(self):
@@ -78,7 +110,7 @@ class JointingStep(Step):
         super().__init__(self.name)
 
 
-class PaddingStep(Step):
+class PaddingStep(AlterationStep):
     """Used in the Sofa and Chair and Bed to make things comfortable"""
     def __init__(self):
         self.name = 'Padding'
@@ -86,7 +118,7 @@ class PaddingStep(Step):
         super().__init__(self.name)
 
 
-class PlaningStep(Step):
+class PlaningStep(AlterationStep):
     """Smoothing out and making parallel the two broad sides of a board.
     Useful when making desks, tables, cutting boards
     """
@@ -96,7 +128,7 @@ class PlaningStep(Step):
         super().__init__(self.name)
 
 
-class RoutingStep(Step):
+class RoutingStep(AlterationStep):
     """Rounding over corners and making inlays and stuff"""
     def __init__(self):
         self.name = 'Routing'
@@ -104,7 +136,7 @@ class RoutingStep(Step):
         super().__init__(self.name)
 
 
-class SandingStep(Step):
+class SandingStep(AlterationStep):
     """Removing rough edges and making the exposed, viewable faces presentable. Used by most 'finished' goods"""
     def __init__(self):
         self.name = 'Sanding'
@@ -112,7 +144,7 @@ class SandingStep(Step):
         super().__init__(self.name)
 
 
-class StainingStep(Step):
+class StainingStep(AlterationStep):
     """Prettifying the wood. Also protects it from the elements and from food stains and such like"""
     def __init__(self):
         self.name = 'Staining'
@@ -120,7 +152,7 @@ class StainingStep(Step):
         super().__init__(self.name)
 
 
-class TurningStep(Step):
+class TurningStep(GenerationStep):
     """Turning things like bed posts, chair legs, table legs, desk legs, and generally anything which is cylindrical"""
     def __init__(self):
         self.name = 'Turning'
@@ -130,10 +162,13 @@ class TurningStep(Step):
 
 class StepError(Exception):
     """Not sure how this will be used but I have it here in case I think of a reason"""
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs);
+    def __init__(self, *args):
+        super().__init__(*args)
 
 
 if __name__ == '__main__':
     glueing = GluingStep()
     glueing.perform()
+
+    staining = StainingStep()
+    staining.perform()
